5cca171285dd55287efd29fe1e27b6549fb155c1
Jakub Polak <jackob50505@gmail.com>
2026-06-07 17:26:46 +0200

    commit

--- APBD_Trening1/Controllers/PcController.cs
+++ APBD_Trening1/Controllers/PcController.cs
@@ -1,15 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APBD_Trening1.Data;
+using APBD_Trening1.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace APBD_Trening1.Controllers;
 
 //Dziedziczy po ControllerBase
-[Route("api/[controller]")]
+[Route("api/pcs")]
 [ApiController]
 public class PcController : ControllerBase
 {
-    [HttpGet]
-    public IActionResult Get()
+    
+    //Ustawiamy kontekst bazy danych
+    private readonly AppDbContext _dbService;
+
+    public PcController(AppDbContext dbContext)
     {
-        return Ok();
+        _dbService = dbContext;
+    }
+    
+    
+    //Get na wszystkie listy, wykonywany asynchronicznie - wszystko musi być async lub mieć await
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var res = await _dbService.PCs.ToListAsync();
+        return Ok(res);
     }
 }
